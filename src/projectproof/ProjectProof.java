1b845db158791ca0f9d43c6776c63a18d5e422cc
David A. Cohen <dcohen@clemson.edu>
2011-11-21 21:17:09 -0500

    Edited ProjectProof to include a Java FileChooser. Sort of.

--- src/projectproof/ProjectProof.java
+++ src/projectproof/ProjectProof.java
@@ -4,30 +4,66 @@
  */
 package projectproof;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 /**
- * Main driver for ProjectProof. Takes input from a file specified at run-time.
- * Requires and ensures clauses from input file must span only one line.
+ * <p>Main driver for {@code ProjectProof}. Takes input from a file specified at
+ * runtime from the file chooser. The input file may include {@link
+ * Definition}s, {@link Operation}s, and {@link Procedure}s in the form
+ * specified in their respective classes. Requires and ensures clauses from the
+ * input file must span only one line.</p>
  *
  */
 public class ProjectProof {
 
     /**
-     * Opens the file specified and adds it to the Environment.
-     * Prints a proof table of each procedure found in the Environment.
+     * <p>Opens the file specified and adds it to the {@code Environment}.
+     * Prints a proof table of each <i>procedure</i> found in the
+	 * {@code Environment}.</p>
      */
     public static void main(String[] args) {
-        if(args.length!=1)
+       /* if(args.length!=1)
         {
             System.err.println("Not enough parameters!\n\t" +
                     "usage: java ProjectProof [formal specification file.]");
             System.exit(1);
         }
-        String filename = args[0];
-        Environment.setFilePath(filename);
-        Environment mainEnv = Environment.getInstance();
-        for(Procedure p : mainEnv.getProcedures()){
-            ProofTable table = new ProofTable(p);
-            System.out.println(table);
+		*/
+
+		try{
+			File inputFile = null;
+			JFileChooser chooser = new JFileChooser();
+					JFrame frame = new JFrame("applicationFrame");
+					frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+					chooser.setDialogTitle("Please choose an input file.");
+					int returnVal = chooser.showOpenDialog(frame);
+				if(returnVal == JFileChooser.APPROVE_OPTION) {
+					inputFile = chooser.getSelectedFile();
+				}
+				else
+					frame.dispose();
+			if(inputFile != null){
+				String filename = inputFile.getAbsolutePath();
+				Environment.setFilePath(filename);
+				Environment mainEnv = Environment.getInstance();
+				for(Procedure p : mainEnv.getProcedures()){
+					ProofTable table = new ProofTable(p);
+					System.out.println(table);
+				}
+			}
+		}
+		catch (NullPointerException npe) {
+			System.err.println("There was an error with the file path.");
+		}
+		catch (IllegalArgumentException iae) {
+			System.err.println("There was an error with the file path.");
+		}
+
+        // String filename = args[0];
+
         }
-    }
+
 }
